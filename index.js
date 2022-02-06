ddaa8c505e8f458f5fd534831a1116b7788ba356
Erin Van Brunt <erinmontybruce@gmail.com>
2022-02-05 17:50:50 -0800

    completed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+//Question 1
+let employee = {};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  employee = {
+    name: `Sam`,
+    streetAddress: `11 Broadway`,
+  };
+  return employee;
+}
+
+//Question 2
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+destructivelyUpdateEmployeeWithKeyAndValue(employee, `name`, `Eli`);
+
+//Question 3
+function deleteFromEmployeeByKey(employee, key) {
+  let { [key]: omit, ...res } = employee;
+  return res;
+}
+
+//Question 4
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
